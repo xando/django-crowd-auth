512f84286f2891ebed641a5bee9e94c3b7dffbb1
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-03-16 17:14:55 +0100

    fix imports

--- tests/tests.py
+++ tests/tests.py
@@ -12,7 +12,7 @@ class TestCrowdSyncUserBackend(TestCase):
             u'first-name': 'test1',
             u'last-name': 'test2'}
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_USER', True)
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_USER', True)
     @patch('crowd_auth.backend.CrowdBackend.crowd_user')
     def test_sync_user_1(self, crowd_user):
         crowd_user.return_value = self.data
@@ -25,7 +25,7 @@ class TestCrowdSyncUserBackend(TestCase):
         self.assertEqual(user.first_name, 'test1')
         self.assertEqual(user.last_name, 'test2')
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_USER', False)
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_USER', False)
     @patch('crowd_auth.backend.CrowdBackend.crowd_user')
     def test_sync_user_2(self, crowd_user):
         crowd_user.return_value = self.data
@@ -47,7 +47,7 @@ class TestCrowdSyncGroupsBackend(TestCase):
         {u'name': u'users'}]
     }
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
     @patch('crowd_auth.backend.CrowdBackend.crowd_get_groups')
     def test_set_groups_1(self, crowd_get_groups):
         crowd_get_groups.return_value = self.data
@@ -64,7 +64,7 @@ class TestCrowdSyncGroupsBackend(TestCase):
         self.assertTrue(admins in user.groups.all())
         self.assertTrue(other not in user.groups.all())
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', False)
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', False)
     @patch('crowd_auth.backend.CrowdBackend.crowd_get_groups')
     def test_set_groups_2(self, crowd_get_groups):
         crowd_get_groups.return_value = self.data
@@ -77,8 +77,8 @@ class TestCrowdSyncGroupsBackend(TestCase):
 
         self.assertEqual(user.groups.count(), 0)
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
-    @patch('crowd.AUTH_CROWD_GROUP_MAP', {'admins': 'gods'})
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
+    @patch('crowd_auth.AUTH_CROWD_GROUP_MAP', {'admins': 'gods'})
     @patch('crowd_auth.backend.CrowdBackend.crowd_get_groups')
     def test_set_groups_map(self, crowd_get_groups):
         crowd_get_groups.return_value = self.data
@@ -91,8 +91,8 @@ class TestCrowdSyncGroupsBackend(TestCase):
         self.assertEqual(user.groups.count(), 1)
         self.assertTrue(gods in user.groups.all())
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
-    @patch('crowd.AUTH_CROWD_SUPERUSER_GROUP', 'admins')
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
+    @patch('crowd_auth.AUTH_CROWD_SUPERUSER_GROUP', 'admins')
     @patch('crowd_auth.backend.CrowdBackend.crowd_get_groups')
     def test_set_groups_superuser(self, crowd_get_groups):
         crowd_get_groups.return_value = self.data
@@ -103,8 +103,8 @@ class TestCrowdSyncGroupsBackend(TestCase):
         self.assertEqual(user.groups.count(), 1)
         self.assertEqual(user.is_superuser, True)
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
-    @patch('crowd.AUTH_CROWD_STAFF_GROUP', 'users')
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
+    @patch('crowd_auth.AUTH_CROWD_STAFF_GROUP', 'users')
     @patch('crowd_auth.backend.CrowdBackend.crowd_get_groups')
     def test_set_groups_staff(self, crowd_get_groups):
         crowd_get_groups.return_value = self.data
@@ -115,8 +115,8 @@ class TestCrowdSyncGroupsBackend(TestCase):
         self.assertEqual(user.groups.count(), 1)
         self.assertEqual(user.is_staff, True)
 
-    @patch('crowd.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
-    @patch('crowd.AUTH_CROWD_CREATE_GROUPS', True)
+    @patch('crowd_auth.AUTH_CROWD_ALWAYS_UPDATE_GROUPS', True)
+    @patch('crowd_auth.AUTH_CROWD_CREATE_GROUPS', True)
     @patch('crowd_auth.backend.CrowdBackend.crowd_get_groups')
     def test_set_groups_create(self, crowd_get_groups):
         crowd_get_groups.return_value = self.data
